acf001e0871147ae99515d985d82485afc8b2f2b
mahashree raja <mahashree raja>
2017-07-23 17:20:23 +0530

    commit done

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-   res.send('Hello World');
-})
+    res.send("Hello dev, am a local route nobody knows me yet!");
+});
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+var server = app.listen(port, function () {
+    console.log('node server is just fine! and running on port - ' + port);
+});
