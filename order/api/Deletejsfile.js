7b47b9da519e2e98a17c75308ac6ec93ad7d210d
Mahashree Raja <mahashree.raja@avon.com>
2017-10-26 17:49:22 +0530

    sdcvdv

--- order/api/Deletejsfile.js
+++
@@ -1,4 +0,0 @@
-exports.Delete = function(){
-
-console.log("inside api save js delete fn ");
-}
--- order/ordertest.js
+++ order/ordertest.js
@@ -1,6 +1,7 @@
 var requireDirectory = require('require-directory');
-var apiPath = require('./api');
-var api =apiPath.normalize("..//api");
+var api = requireDirectory('./api/index.js');
+console.log(api);
+//var api =apiPath.normalize("..//api");
 //var api = require('./api');
 api.save.chkfn();
 api.Deletejsfile.Delete();
