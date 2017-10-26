8ad74b6f673a30cf6a3d794ac661fd8a22a3d124
Mahashree Raja <mahashree.raja@avon.com>
2017-10-26 12:33:16 +0530

    change done

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-var chkval = require('./api');
+var requireDirectory = require('require-directory');
+var chkval = requireDirectory(module, './api');
+
+
+//var chkval = require('./api');
 console.log(chkval);
 console.log("Hello world");
