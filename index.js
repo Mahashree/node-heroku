302ca5cb792e2d26462aac0bb904b321bf550026
Mahashree Raja <mahashree.raja@avon.com>
2017-10-26 13:11:31 +0530

    jbjkllk

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var chkval = requireDirectory(module, './api');
 var order = requireDirectory(module, './order');
 
 //var chkval = require('./api');
-console.log(chkval);
+//console.log(chkval);
 console.log(order.ordertest);
-console.log("Hello world");
+//console.log("Hello world");
