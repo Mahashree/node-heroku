ce97d938ccbe7f03eb30b9fb96dd52b7ad5c4f95
Mahashree Raja <mahashree.raja@avon.com>
2017-10-26 18:19:02 +0530

    fefsdsdsdwwdwd

--- order/ordertest.js
+++ order/ordertest.js
@@ -1,7 +1,9 @@
 var requireDirectory = require('require-directory');
-var directory =__dirname.slice(0,__dirname.lastIndexOf('/'))+"/api";
+var directory ="";
+directory+=__dirname.slice(0,__dirname.lastIndexOf('/'));
 console.log(directory);
-var api = require('/api');
+
+var api = requireDirectory(directory,'./api');
 console.log(api);
 //var api =apiPath.normalize("..//api");
 //var api = require('./api');
