bc0b914ba5b959d46000b19c84e96b6f5dc102ad
Mahashree Raja <mahashree.raja@avon.com>
2017-10-26 12:58:50 +0530

    fvsdfgv

--- order/ordertest.js
+++ order/ordertest.js
@@ -1,4 +1,5 @@
-var requireDirectory = require('require-directory');
-var api = requireDirectory(module, './api');
+//var requireDirectory = require('require-directory');
+//var api = requireDirectory(module, './api');
+var api = require('./api');
 api.save.chkfn();
 api.Deletejsfile.Delete();
