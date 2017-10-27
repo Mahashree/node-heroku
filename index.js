c8e06265973968fffd1023099961cf72cd1724a2
Mahashree Raja <mahashree.raja@avon.com>
2017-10-27 09:32:25 +0530

    dvdfvdfvb

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var requireDirectory = require('require-directory');
 var chkval = requireDirectory(module, './api');
-//var order = requireDirectory(module, './order');
+var order = require('/../order');
 //console.log(module);
 //var chkval = require('./api');
 console.log(chkval);
-console.log(order.ordertest);
+console.log(order);
 //console.log("Hello world");
