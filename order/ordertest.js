2cf87ce89995ecd0b575a5b4163e4c6fccef1afa
Mahashree Raja <mahashree.raja@avon.com>
2017-10-27 09:48:55 +0530

    cscscsc

--- order/ordertest.js
+++ order/ordertest.js
@@ -3,10 +3,10 @@
 directory+=__dirname.slice(0,__dirname.lastIndexOf('/'));
 console.log(directory);*/
 
-//var api = requireDirectory(module,'/../api');
-console.log(require.main.filename);
+var api = require('/../api');
+//console.log(require.main.filename);
 //var api =apiPath.normalize("..//api");
 //var api = require('./api');
-//api.save.chkfn();
-//api.Deletejsfile.Delete();
+api.save.chkfn();
+api.Deletejsfile.Delete();
 //console.log("idheebjebrf");
