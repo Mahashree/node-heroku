8b3c1e48937190c0425e7317376021cd147d8c63
Mahashree Raja <mahashree.raja@avon.com>
2017-10-27 11:32:46 +0530

    cxsfcvxcvZ

--- api/save.js
+++ api/save.js
@@ -1,6 +1,6 @@
 exports.chkfn = function(){
  var requireDir  = require('require-dir');
- var Deletejsfile = requireDir('./Deletejsfile.js');
- console.log(Deletejsfile.Delete);
+ var api = requireDir('./api');
+ console.log(api.Deletejsfile.Delete());
 console.log("inside api save js chkfn ");
 }
