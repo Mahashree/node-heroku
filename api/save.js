5ae9e055a4b996dd30815f8b9dd7fb6527fb840f
Mahashree Raja <mahashree.raja@avon.com>
2017-10-27 12:09:25 +0530

    fdgvdgdgdg

--- api/save.js
+++ api/save.js
@@ -2,6 +2,7 @@ exports.chkfn = function(){
  //var requireDir  = require('require-dir');
  var fs = require('fs');
  console.log(fs.readdirSync('api',{}));
+ console.log(fs.statSync('./api'));
  //var Deletejsfile = require('Deletejsfile.js');
  //console.log(Deletejsfile);
  //console.log(Deletejsfile.Delete());
