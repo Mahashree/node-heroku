c27a1e5d755fe99e13397259768418655cf283ba
Mahashree Raja <mahashree.raja@avon.com>
2017-10-27 11:26:36 +0530

    cedcdcv

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ var requireDirectory = require('require-directory');
 var api = requireDirectory(module,'./api');
 
 //chkval.exports = require('./api')();
-console.log(api.save.chkfn);
+console.log(api.save.chkfn());
