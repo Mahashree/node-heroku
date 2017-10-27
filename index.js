d16b938a14b7b340299060d44413d9aa07121f4f
Mahashree Raja <mahashree.raja@avon.com>
2017-10-27 16:08:48 +0530

    ererer

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //var requireDirectory = require('require-directory');
 //var api1 = requireDirectory(module,'./api');
-//var requireDir = require('require-dir');
+var requireDir = require('require-dir');
 
 //exports.route = require('require-dir')('./api');
-var requireDir = require('./api');
+//var requireDir = require('./api');
 console.log("Step1");
 console.log(requireDir);
 console.log(requireDir.save.getMsg());
