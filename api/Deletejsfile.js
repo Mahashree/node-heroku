30da69d0c81b48a4d2a1097a4f4af22c024008cf
Mahashree Raja <mahashree.raja@avon.com>
2017-10-27 17:12:21 +0530

    dfdfdf

--- api/Deletejsfile.js
+++ api/Deletejsfile.js
@@ -1,5 +1,5 @@
 exports.Delete = function(){
-	var modules = require('./test.js');
+	var modules = require('../test.js');
 	 
 	console.log("inside api Delete js delete fn ");
 	modules.save1.getMsg();
