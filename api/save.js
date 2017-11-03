c51b375e7a3e81d627edd3b5f61825ae046ece59
Mahashree Raja <mahashree.raja@avon.com>
2017-11-03 12:40:29 +0530

    reverted

---
+++ api/save.js
@@ -0,0 +1,17 @@
+exports.getMsg = function(){
+	var modules = require('../test.js');
+	
+	console.log("inside api getMsg Function1 ");
+	modules.order1.getOrder();
+ //var requireDir  = require('require-dir');
+ /*var fs = require('fs');
+ console.log(fs.readdirSync('api',{}));
+ console.log(fs.statSync('./api'));*/
+ //var Deletejsfile = require('Deletejsfile.js');
+ //var routedel = route.Deletejsfile;
+ //console.log("Step1");
+ //console.log(routedel);
+ //console.log("Step2");
+ //console.log(Deletejsfile.Delete());
+
+}
