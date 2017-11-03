00e61d7c00a82845716eea369ef85a7f68df02d8
Mahashree Raja <mahashree.raja@avon.com>
2017-11-03 14:31:17 +0530

    rrrrrrrrrrrrrrrrrrr

--- validation/getErrDescription.js
+++
@@ -1,5 +0,0 @@
-exports.getErrDescription = function (errCode){
-	var errProperties = PropertiesReader('fbbot-tr-errProperties.properties');
-	var errDesc = errProperties.get(errCode);
-	return errDesc;
-}
