1ffe3d231d4f1322b192ef1c48415ef554586687
Mahashree Raja <mahashree.raja@avon.com>
2017-11-06 14:35:05 +0530

    xcvsdvd

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
-console.log("Inside API");/*
+console.log("Inside API");
 var PropertiesReader = require('properties-reader');
 var properties = PropertiesReader('facebookbot-tr.properties');
-
+console.log("After PropertiesReader");
+console.log(properties);
 exports.wshost = properties.get('fbbot.ws.tr.host');
 exports.wsport = properties.get('fbbot.ws.tr.port');
 exports.wsmethodType = properties.get('fbbot.ws.tr.methodtype');
@@ -14,7 +15,7 @@ exports.redisUrl=properties.get('fbbot.redis.tr.redisUrl');
 exports.redisAuth_pass=properties.get('fbbot.redis.tr.redisAuth_pass');
 exports.redisServername =properties.get('fbbot.redis.tr.redisServername');
 
-
+/*
 
 exports.prepareWSDetails= function(type, data) {
 
@@ -109,4 +110,4 @@ exports.prepareWSDetails= function(type, data) {
         }
     };
     return options;
-}*/
+}
--- module.js
+++ module.js
@@ -1,6 +1,6 @@
 console.log("Inside module.js");
 var requireDirectory = require('require-directory');
-console.log(requireDirectory(module,'./api'));
+//console.log(requireDirectory(module,'./api'));
 module.exports.pathDetails = {
 	
 	//api
