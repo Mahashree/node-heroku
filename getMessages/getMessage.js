b1c750127f6ef326b5012d3ab11616252e950a34
Mahashree Raja <mahashree.raja@avon.com>
2017-11-08 11:39:08 +0530

    xcdxvdfv

--- getMessages/getMessage.js
+++ getMessages/getMessage.js
@@ -1,6 +1,10 @@
+console.log("Inside getMessages");
+var PropertiesReader = require('properties-reader');
+console.log(PropertiesReader);
+var getMessagesProperties = PropertiesReader('./en_us_Message.properties');
+console.log(getMessagesProperties);
 exports.getMessages = function(event,msgkey){
-	var PropertiesReader = require('properties-reader');
-	var getMessagesProperties = PropertiesReader('./en_us_Message.properties');
+	
 	var  msgvalue= getMessagesProperties.get(msgkey);
 	return msgvalue;
 }
