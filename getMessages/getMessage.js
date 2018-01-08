c269ac7a4398c136015920ad17ce4351631c531d
Mahashree Raja <mahashree.raja@avon.com>
2018-01-08 15:53:52 +0530

    kmcwkmxc

--- getMessages/getMessage.js
+++
@@ -1,6 +0,0 @@
-var PropertiesReader = require('properties-reader');
-var getMessagesProperties = PropertiesReader('./getMessages/tr_Message.properties');
-exports.getMessages = function(msgkey){	
-	var  msgvalue= getMessagesProperties.get(msgkey);	
-	return msgvalue;
-}
--- validation/getErrDescription.js
+++
@@ -1,6 +0,0 @@
-exports.getErrDescription = function (errCode){
-	var PropertiesReader = require('properties-reader');
-	var errProperties = PropertiesReader('./config/fbbot_tr_errproperties_trlang.properties');
-	var errDesc = errProperties.get(errCode);
-	return errDesc;
-}
