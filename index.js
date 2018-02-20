f23196d9969371f8327b1b150208c08c8873ca79
Mahashree Raja <mahashree.raja@avon.com>
2018-02-20 16:12:55 +0530

    ujiuhi

--- index.js
+++ index.js
@@ -4,28 +4,21 @@ http.createServer(function(req, res) {
   res.writeHead(200, {
     'Content-Type': 'text/html'
   });
-  res.write('<!DOCTYPE html>
-<html>
-<title>Web view</title>
-<body>
-<h2>Hello Mahashree!!</h2>
-<script>
+  res.write('<!DOCTYPE html>\n<html>\n<title>Web view</title>\n'+
+'\n<body>\n<h2>Hello Mahashree!!</h2>\n'+
+'\n<script>\n(function(d, s, id){'+
+  '\nvar js, fjs = d.getElementsByTagName(s)[0];'+
+  '\nif (d.getElementById(id)) {return;}'+
+ '\n js = d.createElement(s); js.id = id;'+
+  '\njs.src = "//connect.facebook.com/en_US/messenger.Extensions.js";'+
+  '\nfjs.parentNode.insertBefore(js, fjs);'+
+'\n}(document, ''script'', ''Messenger''));'+
 
-(function(d, s, id){
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {return;}
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.com/en_US/messenger.Extensions.js";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'Messenger'));
-
-</script>
-</body>
-</html>');
+'\n</script>\n'+
+'\n</body>'+
+'\n</html>');
   res.end();
-}).listen(8888, '127.0.0.1');
+}).listen(3000, 'localhost');
 console.log('Server running at http://127.0.0.1:8888');
-
-
 
 
