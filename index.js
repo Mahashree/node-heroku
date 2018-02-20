c7a46de5c12a1428972bf67ee8a2da8a150a7c3f
Mahashree Raja <mahashree.raja@avon.com>
2018-02-20 15:49:59 +0530

    gljhbkl

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-﻿console.log("Prompt-choices");
-var Choices = require('prompt-choices');
-var choices = new Choices(['foo', 'bar', 'baz']);
-console.log("choices");
-console.log(choices);
+﻿
+<!DOCTYPE html>
+<html>
+<title>Web view</title>
+<body>
+<h2>Hello Mahashree!!</h2>
+<script>
+
+(function(d, s, id){
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.com/en_US/messenger.Extensions.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'Messenger'));
+
+</script>
+</body>
+</html>
