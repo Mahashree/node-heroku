dff4633662dabc3bee97214776132994e13315a3
Mahashree Raja <mahashree.raja@avon.com>
2018-02-22 15:57:58 +0530

    check

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
-﻿
-var http = require('http');  
-http.createServer(function(req, res) {  
-  res.writeHead(200, {
-    'Content-Type': 'text/html'
+var express = require('express');
+var app = express();
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function (req, res) {
+	
+res.set({
+  'Content-Type': 'text/plain',  
+  'X-Frame-Options': 'ALLOW-FROM https://www.messenger.com/'
+});
+    res.sendFile(__dirname +'/'+'index.html');
+	//res.location('https://smidalms.herokuapp.com/');
+	
+  /*var options = {    
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true,
+		'X-Frame-Options':'ALLOW-FROM https://www.messenger.com/'
+    }
+  };
+
+  var fileName = __dirname +'/'+'index.html';
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      next(err);
+    } else {
+      console.log('Sent:', fileName);
+    }
   });
-  res.write('<!DOCTYPE html>\n<html>\n<title>Web view</title>\n'+
-'\n<body>\n<h2>Hello Mahashree!!</h2>\n'+
-'\n<script>\n(function(d, s, id){'+
-  '\nvar js, fjs = d.getElementsByTagName(s)[0];'+
-  '\nif (d.getElementById(id)) {return;}'+
- '\n js = d.createElement(s); js.id = id;'+
-  '\njs.src = "//connect.facebook.com/en_US/messenger.Extensions.js";'+
-  '\nfjs.parentNode.insertBefore(js, fjs);'+
-'\n}(document, '+'script'+', '+'Messenger'+'));'+
 
-'\n</script>\n'+
-'\n</body>'+
-'\n</html>');
-  res.end();
-}).listen(3000, 'localhost');
-console.log('Server running at http://127.0.0.1:8888');
+});*/
+	
+	
+	
+	
+});
 
-
+/*app.post('/submit-student-data', function (req, res) {
+    var name = req.body.firstName + ' ' + req.body.lastName;
+    
+    res.send(name + ' Submitted Successfully!');
+});*/
