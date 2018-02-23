193d7c238bc7165180270843f7b7ed6e04926dec
Mahashree Raja <mahashree.raja@avon.com>
2018-02-23 14:49:59 +0530

    chk

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
 'use strict';
+
 // ================================================================
 // get all the tools we need
 // ================================================================
 var express = require('express');
 var routes = require('./routes/index.js');
 var port = process.env.PORT || 3000;
+
 var app = express();
+
 // ================================================================
 // setup our express application
 // ================================================================
 app.use('/public', express.static(process.cwd() + '/public'));
 app.set('view engine', 'ejs');
+
+
 // ================================================================
 // setup routes
 // ================================================================
 routes(app);
+
 // ================================================================
 // start our server
 // ================================================================
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Node server is running***');
+app.listen(port, function() {
+    console.log('Server listening on port ' + port + '...');
 });
