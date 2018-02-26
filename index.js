3c74ba43900655e67bd388ad456edcc089af954f
Mahashree Raja <mahashree.raja@avon.com>
2018-02-26 12:25:04 +0530

    gffg

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ var http=require('https');
 //make the request object
 var request=http.request({
   'host': 'https://obscure-stream-93442.herokuapp.com',
-  'port': process.env.PORT,
+  'port': 3000,
   'path': '/getRepData',
   'method': 'GET'
 });
