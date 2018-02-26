5c36e55d483db539a0457c7053203558334f821c
Mahashree Raja <mahashree.raja@avon.com>
2018-02-26 16:00:44 +0530

    dfsdfdf

--- index.js
+++ index.js
@@ -4,20 +4,44 @@ var app = express();
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 
-/*var server = app.listen(process.env.PORT || 3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
     console.log('Node server is running***');
-});*/
+});
+/*
 //var app = require('express')();
 var server = require('http').createServer(app);
 server.listen(process.env.PORT || 3000);
 var io = require('socket.io')(server);
-io.on('connection', function(){  });
+//io.on('connection', function(){  });
 
-
-app.get('/', function (req, res) {
-
-
+var socket = io.connect('obscure-stream-93442.herokuapp.com');
+socket.on('connect', function(data) {
+    socket.emit('join', 'Hello World from client');
+});
+ */
 	
+app.get('/', function (req, res) {
+// Connect to server
+var io = require('socket.io-client')
+var socket = io.connect('obscure-stream-93442.herokuapp.com', {reconnect: true});
+
+console.log('2');
+
+// Add a connect listener
+socket.on('connect', function(socket) { 
+    console.log('Connected!');
+});
+	
+
+});
+
+/*app.post('/submit-student-data', function (req, res) {
+    var name = req.body.firstName + ' ' + req.body.lastName;
+    
+    res.send(name + ' Submitted Successfully!');
+	
+	
+});
 var https=require('http');
 
 //
@@ -53,7 +77,7 @@ var reqGet = https.request(optionsget, function(res) {
 catch(ex){
 	console.log(ex);
 }
-/*//reqGet.end();
+//reqGet.end();
 reqGet.on('error', function(e) {
     console.error(e);
 });
@@ -80,13 +104,10 @@ request.on('response', function(response) {
    });
 });*/
     //res.sendFile(__dirname +'/'+'index.html');
-});
 
-/*app.post('/submit-student-data', function (req, res) {
-    var name = req.body.firstName + ' ' + req.body.lastName;
-    
-    res.send(name + ' Submitted Successfully!');
-});*/
+
+
+*/
 
 
 
