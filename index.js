1920d033fef72f282eb852f737e83b6c407843b8
Mahashree Raja <mahashree.raja@avon.com>
2018-02-26 12:02:08 +0530

    k.hdwkldhwidh

---
+++ index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function (req, res) {
+	
+    res.sendFile(__dirname +'/'+'index.html');
+});
+
+/*app.post('/submit-student-data', function (req, res) {
+    var name = req.body.firstName + ' ' + req.body.lastName;
+    
+    res.send(name + ' Submitted Successfully!');
+});*/
+
+var server = app.listen(process.env.PORT || 3000, function () {
+    console.log('Node server is running***');
+});
+
+
+var http=require('http');
+
+//make the request object
+var request=http.request({
+  'host': 'https://obscure-stream-93442.herokuapp.com',
+  'port': 80,
+  'path': '/getRepData',
+  'method': 'GET'
+});
+
+//assign callbacks
+request.on('response', function(response) {
+   console.log('Response status code:'+response.statusCode);
+
+   response.on('data', function(data) {
+     console.log('Body: '+data);
+   });
+});
