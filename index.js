3b525203ec8ad9f9f557f6859f9823f4c4f77e33
Mahashree Raja <mahashree.raja@avon.com>
2018-02-27 14:59:21 +0530

    gvhfhvbnvc

--- index.js
+++ index.js
@@ -11,16 +11,18 @@ var server = app.listen(process.env.PORT || 3000, function () {
 	
 app.get('/', function (req, res) {
 //res.sendFile(__dirname +'/'+'index.html');
-var io = require('socket.io-client')
+var io = require('socket.io-client');
 var socket = io.connect('obscure-stream-93442.herokuapp.com/', {reconnect: true});
 
 console.log('2');
 
 socket.on('connect', function(data) {
     socket.emit('join', 'Hello World from client');
-		});
+	
+	});
 socket.on('messages', function(data) {
                 console.log(data);
         });
+
 });
 
