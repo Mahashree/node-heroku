420f8c17a8823bd1f56adede0884ad894ea37414
Mahashree Raja <mahashree.raja@avon.com>
2018-03-02 11:47:10 +0530

    fgfgfg

--- index.js
+++ index.js
@@ -8,41 +8,44 @@ var server = app.listen(process.env.PORT || 3000, function () {
     console.log('Node server is running***');
 });
 var senderId;
-	
-
-
 var redisPort =6380;
 var redisUrl= 'azupsdsstred1.redis.cache.windows.net';
 var redisAuth_pass='Ze6sjIhQGTl96rbe+mA3O3hbrskbRdlmpNUIbczv1Oo=';
 var redisServername = 'azupsdsstred1.redis.cache.windows.net';
-var client = redis.createClient(redisPort,redisUrl, {auth_pass: redisAuth_pass, tls: {servername: redisServername}})
+var client = redis.createClient(redisPort,redisUrl, {auth_pass: redisAuth_pass, tls: {servername: redisServername}});
 	
 app.get('/', function (req, res) {
 res.sendFile(__dirname +'/'+'index.html');
 var io = require('socket.io-client');
 var socket = io.connect('obscure-stream-93442.herokuapp.com/', {reconnect: true});
-//var socket = io.connect('//localhost:3001',{'forceNew':true });
+
 console.log('1');
 
 socket.on('connect', function(data) {
 	console.log('2');
-    
+    socket.on('response', function(data) {
+		console.log('3');
+                console.log(data);
+        });
+	
+	});
+	console.log('4');
 	
  socket.on('eventemit', function(data) {
-	 console.log('3');
-                console.log(data);
+	 console.log('5');
+                console.log(data);				
 				senderId= data.event.sender.id;
-				
 				client.hgetall(senderId, function(err, obj) {	
 				 
 				   var redisdata=obj;
 				  console.log("redisdata from Client");
 				   console.log(redisdata);
 					});
-				});
-
-		
-});		
+				
+				
+				
+        });
+});
 /*var Client = require('node-rest-client').Client;
  
 var client = new Client();
@@ -67,5 +70,5 @@ client.registerMethod("jsonMethod", "https://obscure-stream-93442.herokuapp.com/
     console.log(response);
 });*/
 
-});
+
 
