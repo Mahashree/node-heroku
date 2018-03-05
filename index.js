c2b7aab53c44f37231ab3f5c7fd2e3b95ee01d05
Mahashree Raja <mahashree.raja@avon.com>
2018-03-05 16:25:58 +0530

    cvfgrf

--- index.js
+++ index.js
@@ -65,8 +65,9 @@ socket.on('connect', function(data) {
 app.get('/getPendingOrderDetails.js', function (req, res) {
 	console.log(" Inside fn items:");
 	console.log(items);
+	
 	//res.status(200).send(JSON.stringify({items}));
-	res.json(items);
+	res.send(req.query);
 	
 });
 /*var Client = require('node-rest-client').Client;
